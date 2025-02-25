364313f8c9e47a2b4f135bcec60652ca22560fdb
Siddhesh Mahajan <44565090+sidmahajan47@users.noreply.github.com>
2025-02-25 13:25:17 +0530

    HandControlModule.py

--- HandControlModule.py
+++ HandControlModule.py
@@ -3,6 +3,10 @@ import mediapipe as mp
 import time
 import math
 
+"""
+Hand Tracking Module 
+By: Murtaza Hassan
+"""
 
 class handDetector:
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
